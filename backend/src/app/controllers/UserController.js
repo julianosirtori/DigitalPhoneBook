fa7e23dd6fc26a0895dbab8a4cc571e202bc47ee
Juliano Sirtori <julianosirtori@gmail.com>
2019-11-01 09:54:07 -0300

    Implemented UserController

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,6 +1,56 @@
+import User from '../models/User';
+
 class UserController {
-    index(req, res) {
-        res.json({ message: 'Controller User' });
+    async index(req, res) {
+        const users = await User.findAll({
+            attributes: ['id', 'name', 'email'],
+        });
+
+        return res.json(users);
+    }
+
+    async store(req, res) {
+        const userExists = await User.findOne({
+            where: { email: req.body.email },
+        });
+        if (userExists) {
+            return res.status(400).json({ error: 'User  already exists' });
+        }
+        const { id, name, email } = await User.create(req.body);
+        return res.json({ id, name, email });
+    }
+
+    async show(req, res) {
+        const { id } = req.params;
+        const user = await User.findByPk(id);
+        if (!user) {
+            return res.status(401).json({ error: 'User not found' });
+        }
+        const { email, name } = user;
+        return res.json({ id, name, email });
+    }
+
+    async update(req, res) {
+        const { id } = req.params;
+        const user = await User.findByPk(id);
+        if (!user) {
+            return res.status(401).json({ error: 'User not found' });
+        }
+
+        const { name, email } = await user.update(req.body);
+
+        return res.json({ id, name, email });
+    }
+
+    async delete(req, res) {
+        const { id } = req.params;
+        const user = await User.findByPk(id);
+        if (!user) {
+            return res.status(401).json({ error: 'User not found' });
+        }
+        user.destroy();
+
+        return res.json();
     }
 }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -11,5 +11,9 @@ routes.post('/login', SessionController.store);
 routes.use(authMiddleware);
 
 routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
+routes.get('/users/:id', UserController.show);
+routes.put('/users/:id', UserController.update);
+routes.delete('/users/:id', UserController.delete);
 
 export default routes;

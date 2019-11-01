e60d325d952320789d73fa1e9a13354ced4ed13d
Juliano Sirtori <julianosirtori@gmail.com>
2019-11-01 09:04:25 -0300

    Create Auth Middleware

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/login', SessionController.store);
+routes.use(authMiddleware);
 
 routes.get('/users', UserController.index);
 
